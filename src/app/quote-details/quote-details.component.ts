9859038259c8d6f8e13fe5cf8052eb2207f1c80e
musyoka8 <nagilmusyoka@gmail.com>
2020-06-15 16:43:14 +0300

    new definition of property to take and the event emmited

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -10,7 +10,9 @@ export class QuoteDetailsComponent implements OnInit {
 
   @Input() quote: Quote;
   @Output() isComplete = new EventEmitter<boolean>();
-
+  quoteDelete(complete: boolean){
+    this.isComplete.emit(complete);
+  }
   constructor() { }
 
   ngOnInit() {
