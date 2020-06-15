9984c9e82c2aa4899cdb094a6fad6ac36996ed8f
musyoka8 <nagilmusyoka@gmail.com>
2020-06-15 20:30:30 +0300

    update the form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 import { HighlightquoteDirective } from './highlightquote.directive';
-import { DateCountPipe } from './date-count.pipe';
 import { TimeCountPipe } from './time-count.pipe';
-import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { FormComponent } from './form/form.component';
+
 
 @NgModule({
   declarations: [
@@ -16,9 +17,9 @@ import { QuoteFormComponent } from './quote-form/quote-form.component';
     QuoteComponent,
     QuoteDetailsComponent,
     HighlightquoteDirective,
-    DateCountPipe,
     TimeCountPipe,
-    QuoteFormComponent
+    FormComponent,
+
   ],
   imports: [
     BrowserModule,
--- src/app/form/form.component.spec.ts
+++ src/app/form/form.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { QuoteFormComponent } from './quote-form.component';
+import { FormComponent } from './form.component';
 
-describe('QuoteFormComponent', () => {
-  let component: QuoteFormComponent;
-  let fixture: ComponentFixture<QuoteFormComponent>;
+describe('FormComponent', () => {
+  let component: FormComponent;
+  let fixture: ComponentFixture<FormComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ QuoteFormComponent ]
+      declarations: [ FormComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(QuoteFormComponent);
+    fixture = TestBed.createComponent(FormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
