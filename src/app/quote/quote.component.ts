7945f783469da02d263b5c4efc5daa37d5098c71
musyoka8 <nagilmusyoka@gmail.com>
2020-06-15 16:29:53 +0300

    add delete button to quote detail html:

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,12 +8,11 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes: Quote[] = [
-    // tslint:disable-next-line: whitespace
-    {id: 1,quote:'\"Mobile is the enabling centerpiece of digital convergence. Mobile is the glue for all other digital industries to use when approaching convergence, but mobile is also the digital gateway for the real world to join in this global metamorphosis of human behavior."'},
-    {id: 2, quote: '\"I think the biggest change, and the one we’re already starting to see take shape, is that globally the majority of internet usage will be done via a mobile device and for most people the mobile web will be their primary – if not their only – way of experiencing the internet."'},
-    {id: 3, quote: '\"Mobile is becoming not only the new digital hub, but also the bridge to the physical world. That’s why mobile will affect more than just your digital operations — it will transform your entire business."'},
-    {id: 4, quote: '\"The future of mobile is the future of online. It is how people access online content now."'},
-    {id: 5, quote: '\"Our lives will be facilitated by a myriad of adaptive applications running on different devices, with different sensors, all of them collecting tidbits about everything we do, and feeding big digital brains that can adapt applications to our needs simply because they get to know us."'},
+    new Quote(1, 'Mobile is the enabling centerpiece of digital convergence. Mobile is the glue for all other digital industries to use when approaching convergence, but mobile is also the digital gateway for the real world to join in this global metamorphosis of human behavior.', 'Brian Kefa', 'Quoted by  Daniel Bæk, Co-founder of Nodes', 0, 0, new Date(2019,8,20)),
+    new Quote(2, 'I think the biggest change, and the one we’re already starting to see take shape, is that globally the majority of internet usage will be done via a mobile device and for most people the mobile web will be their primary – if not their only – way of experiencing the internet.', 'Peninah Njeri', 'Quoted by Tomi Ahonen, consultant and speaker', 0, 0, new Date(2019,7,31)),
+    new Quote(3, 'Mobile is becoming not only the new digital hub, but also the bridge to the physical world. That’s why mobile will affect more than just your digital operations — it will transform your entire business.', 'Cynthia Odul', 'Quoted by  Daniel Bæk, Co-founder of NodesQuoted by Matt Haig Author of Mobile Marketing – “The Message Revolution', 0, 0, new Date(2019,6,20)),
+    new Quote(4, 'The future of mobile is the future of online. It is how people access online content now.', 'Robert Einsten', 'Quoted by Phil Nickinson, Editor of Android Central', 0, 0, new Date(2019,8,22)),
+    new Quote(5, 'Our lives will be facilitated by a myriad of adaptive applications running on different devices, with different sensors, all of them collecting tidbits about everything we do, and feeding big digital brains that can adapt applications to our needs simply because they get to know us.', 'John K9', 'Quoted by Márcio Cyrillo, Executive Director at CI&T', 0, 0, new Date(2019,8,22)),
 
   ];
 
