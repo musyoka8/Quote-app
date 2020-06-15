24f63d3cabdf31882a11a464294fcb618ed79a3e
musyoka8 <nagilmusyoka@gmail.com>
2020-06-15 20:37:12 +0300

    create a new quote object

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-form',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
+  newQuote = new Quote(1, '', '', '', 0, 0, new Date());
 
   constructor() { }
 
